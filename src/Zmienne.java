70697d0645f6a6b609612c93124d6177de47272d
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-06-12 12:49:18 +0200

    Add variables

---
+++ src/Zmienne.java
@@ -0,0 +1,15 @@
+// Zarezerwowane miejsce w pamięci z określonym typem danych
+public class Zmienne {
+
+
+    public static void main(String[] args) {
+        String name = "Aleksander";
+        int firstNumber =  1;
+        int secondNumber = 2;
+        int result = firstNumber + secondNumber;
+        System.out.println(result);
+
+/*        name = "Aga";
+        System.out.println(name);*/
+    }
+}
