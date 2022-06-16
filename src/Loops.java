8e6a2acfd8ebbb86c4daf4be53a1b016dc187f7c
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-06-16 16:26:21 +0200

    Add Loops

---
+++ src/Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+
+    public static void main(String[] args) {
+        int number = 0;
+        while (number<20) {
+            System.out.println("Moja wartość równa się: " + number + " i jest mniejsza od 20");
+            number++;
+        }
+    }
+}
