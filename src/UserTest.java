fa6fbec02aa0d3dadc7f7d6c1cc91b7241a3d1dc
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-07-09 12:07:48 +0200

    Add UserTest

--- src/UserTest.java
+++ src/UserTest.java
@@ -1,9 +1,13 @@
 public class UserTest {
 
     public static void main(String[] args) {
-        User user = new User();
-        user.username = "Bartek";
-        user.password = "QWERTY";
+      //  User user = new User();
+        User user = new User("kasia", "QWERTY");
+
+/*        user.username = "Bartek";
+        user.password = "QWERTY";*/
+        System.out.println(user.username);
+        System.out.println(user.password);
         user.sayHello();
     }
 }
