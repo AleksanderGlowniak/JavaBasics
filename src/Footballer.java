eaa8951a0cce7ade31814c54471e4ac87085bdb5
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-07-30 17:10:31 +0200

    Add Footballer

--- src/Footballer.java
+++ src/Footballer.java
@@ -5,7 +5,13 @@ public class Footballer extends Person {
 
     public Footballer(String name, int age, String footballClub) {
         super(name, age);
+        System.out.println("jestem w konstruktorze footballer");
         this.footballClub = footballClub;
+    }
+
+    public void eat(){
+        System.out.println("I like healthy food");
+
     }
 
     public void playFootball(){
