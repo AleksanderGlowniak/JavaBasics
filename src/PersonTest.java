4e4ce106f73922e9af4f82fd894c2f72707e3fda
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-07-30 16:58:09 +0200

    Add PersonTest

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -1,20 +1,16 @@
 public class PersonTest {
 
     public static void main(String[] args) {
-        MathTeacher teacher = new MathTeacher();
-        teacher.name = "Tom";
-        teacher.age =  29;
-        teacher.school = "Polibuda";
+        MathTeacher teacher = new MathTeacher("Tom", 29, "polibuda");
+
         teacher.walk();
         teacher.eat();
         teacher.sayHello();
         teacher.teachMath();
 
 
-        Footballer footballer = new Footballer();
-        footballer.name = "Mike";
-        footballer.age = 20;
-        footballer.footballClub = "cidroki";
+        Footballer footballer = new Footballer("mike", 20, "cidroki");
+
         footballer.playFootball();
         footballer.walk();
         footballer.eat();
