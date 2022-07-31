8427028a0674daa6f74e91d9960048168cbdf7ab
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-07-31 17:28:50 +0200

    Add ReadOnly

---
+++ src/ReadOnly.java
@@ -0,0 +1,19 @@
+public class ReadOnly {
+
+    private String name = "Olek";
+
+    // gettery pozwalają na pobieranie wartości z pol prywatnych
+
+    /*public String getName() {
+        return name + "jest ok";
+
+    }*/
+
+    // settery umozliwiaja ustawienie pola prywatnego
+    /*public void setName(String name){
+
+        this.name = name;
+    }*/
+
+
+}
