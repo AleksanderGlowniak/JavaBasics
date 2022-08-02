f1548ac145e63abbe762f7480000e050de094286
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-08-02 18:53:38 +0200

    Add ExcelDocument

---
+++ src/Document/ExcelDocument.java
@@ -0,0 +1,10 @@
+package Document;
+
+public class ExcelDocument extends Document{
+
+    @Override
+    public void getDescription() {
+        System.out.println("i am excel document");
+
+    }
+}
