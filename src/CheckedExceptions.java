06e65b9bf35e7496dacf764b96a26fbfd33ceb7f
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-08-03 19:22:57 +0200

    Add CheckedExceptions

--- src/CheckedExceptions.java
+++ src/CheckedExceptions.java
@@ -3,8 +3,16 @@ import java.io.FileNotFoundException;
 
 public class CheckedExceptions {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        readFile("/src/gsrgsr.txt");
+    public static void main(String[] args)  {
+        try {
+            System.out.println("Before reading file");
+            readFile("C:\\Users\\olo88\\Desktop\\javalesson\\src\\test.txt");
+            System.out.println("Closing file");
+        } catch (FileNotFoundException e) {
+            System.out.println("wyjatek zostal wyrzucony");
+            System.out.println(e.getMessage());
+
+        }
     }
     public static void readFile(String path) throws FileNotFoundException {
         FileInputStream fileInputStream = new FileInputStream(path);
